3d0cf15a619ed38ce7502b54feebf72f01239776
Matthias Ruhland <matthias@blockstream.io>
2019-06-27 22:16:28 -0700

    finished construction landing page

--- src/components/image.js
+++ src/components/image.js
@@ -16,9 +16,9 @@ import Img from "gatsby-image"
 const Image = () => {
   const data = useStaticQuery(graphql`
     query {
-      placeholderImage: file(relativePath: { eq: "ocean.jpg" }) {
+      placeholderImage: file(relativePath: { eq: "construction-placeholder.jpg" }) {
         childImageSharp {
-          fluid(maxWidth: 1920, grayscale: true) {
+          fluid(maxWidth: 1920) {
             ...GatsbyImageSharpFluid
           }
         }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,9 @@
 import React from "react"
 
 import Image from "../components/image"
-import { SocialIcon } from 'react-social-icons';
+import { SocialIcon } from "react-social-icons"
 
+import "../components/styles/construction-page.scss"
 
 const Construction = () => {
     return (
@@ -11,22 +12,25 @@ const Construction = () => {
             <div style={{ 
                             display: "flex", 
                             flexDirection: "column", 
-                            justifyContent: "center", 
+                            justifyContent: "space-around", 
                             alignItems: "center", 
                             position: "relative", 
                             zIndex: 1, 
                             height: "100vh", 
-                            color: "#fff" }}>
-                <h1>Sorry! </h1>
-                <h3>I'm currently rebuilding this site!</h3>
-
+                            color: "#fff" }} >
+                <div className="construction-page" style={{ textAlign: "center" }}>
+                    <h1>Sorry!</h1>
+                    <h2>New and improved site</h2>
+                    <h2>is coming soon!</h2>
+                </div>
+                
                 <div style={{ 
                             display: "flex", 
                             flexDirection: "row", 
                             justifyContent: "space-around", 
                             alignItems: "center",
                             width: "200px",
-                            marginTop: "100px" }}>
+                            marginTop: "100px" }} >
                     <SocialIcon url="https://github.com/matthi01" network="github" fgColor="white" />
                     <SocialIcon url="https://ca.linkedin.com/in/matthias-ruhland" network="linkedin" fgColor="white" />
                     <SocialIcon url="https://facebook.com/matthias-ruhland" network="facebook" fgColor="white" />
