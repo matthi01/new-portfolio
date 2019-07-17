437dba3875cc3be51dabccb481ed88aeaa7c0cc0
Matthias Ruhland <matthias@blockstream.io>
2019-07-16 19:00:38 -0700

    image optimizations

--- src/components/ui/avatar/avatar.js
+++ src/components/ui/avatar/avatar.js
@@ -1,11 +1,24 @@
 import React from "react"
 import "./avatar.scss"
+import Img from 'gatsby-image';
+import { useStaticQuery, graphql } from "gatsby"
 
 const avatar = (props) => {
+
+    const data = useStaticQuery(graphql`
+        query {
+            avatar: file(relativePath: { eq: "avatar.png" }) {
+                    childImageSharp {
+                        fluid(maxWidth: 100) {
+                            ...GatsbyImageSharpFluid
+                        }
+                    }
+                }
+            }
+        `)
+
     return (
-        // <div >
-            <img className="avatar" alt="avatar" src={ props.image } />
-        // </div>
+        <Img className="avatar" alt="avatar" fluid={data.avatar.childImageSharp.fluid} />
     )
 }
 
