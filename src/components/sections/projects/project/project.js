b6daad97cf6411f9d8684327c468c0c4d0458bfa
Matthias Ruhland <matthias@blockstream.io>
2019-07-16 19:32:19 -0700

    use gatsby-image for projects sections

--- src/components/sections/projects/project/project.js
+++ src/components/sections/projects/project/project.js
@@ -2,6 +2,7 @@ import React from "react"
 import { FaPlusCircle, FaMinusCircle } from 'react-icons/fa'
 import Fade from 'react-reveal/Fade'
 import "./project.scss"
+import Img from 'gatsby-image';
 
 class Project extends React.Component {
 
@@ -21,7 +22,8 @@ class Project extends React.Component {
                 <div className="project">
                     <div className="project_image">
                         <a href={ this.props.link } target="blank">
-                            <img className="project_image_img" alt={ this.props.alt } src={this.props.image} />
+                            {/* <img className="project_image_img" alt={ this.props.alt } src={this.props.image} /> */}
+                            <Img className="project_image_img" alt="this.props.alt" fluid={ this.props.image } />
                         </a>
                     </div>
                     <div className={ this.state.drawerOpen ? "project_drawer project_drawer_open" : "project_drawer project_drawer_closed" } onClick={ drawerClickHandler }>
--- src/components/sections/projects/projects.js
+++ src/components/sections/projects/projects.js
@@ -2,83 +2,123 @@ import React from "react"
 import "./projects.scss"
 import Project from "./project/project"
 import SectionGradientHeader from "../../ui/section-gradient-header/section-gradient-header"
-import imgBlockstream from "../../../images/blockstream.png"
-import imgBlockExplorer from "../../../images/block-explorer.png"
-import imgETax from "../../../images/etax.png"
-import imgBlockstreamStore from "../../../images/blockstream-store.png"
-import imgSatellite from "../../../images/satellite.png"
-import imgVolkfi from "../../../images/volkfi.png"
-import imgGreen from "../../../images/green.png"
-import imgGentax from "../../../images/gentax.png"
-import imgAmazon from "../../../images/amazon.png"
+import { useStaticQuery, graphql } from "gatsby"
 
 const projects = () => {
 
-    const arrProjects = [
-        {
-            title: "blockstream.com",
-            image: imgBlockstream,
-            description: "Main Blockstream landing page. Built with Jekyll from the ground up as part of Blockstreams rebranding effort.",
-            link: "https://www.blockstream.com",
-            alt: "blockstream.com"
-        },
-        {
-            title: "blockstream.info",
-            image: imgBlockExplorer,
-            description: "Blockstream's block explorer, built using react.",
-            link: "https://www.blockstream.info",
-            alt: "blockstream.info"
-        },
-        {
-            title: "store.blockstream.com",
-            image: imgBlockstreamStore,
-            description: "Blockstream store is a wordpress store that showcases lightning payment technology.",
-            link: "https://store.blockstream.com",
-            alt: "store.blockstream.com"
-        },
-        {
-            title: "volkfi.com",
-            image: imgVolkfi,
-            description: "Volkfi landing page. I was involved as a contractor in the early efforts of building the site.",
-            link: "https://www.volkfi.com",
-            alt: "volkfi.com"
-        },
-        {
-            title: "Blockstream Green (iOS)",
-            image: imgGreen,
-            description: "Blockstream Green iOS wallet app.",
-            link: "https://www.blockstream.com/green",
-            alt: "Blockstream Green (iOS)"
-        }, 
-        {
-            title: "Blockstream Satellite Transmissions App",
-            image: imgSatellite,
-            description: "Blockstream Satellite transmissions app. This web app is available on blockstream.com and allows you to submit files and text messages via the blockstream satellite network. Payments are handled through the lightning network.",
-            link: "https://www.blockstream.com/satellite/",
-            alt: "Blockstream Satellite Transmissions App"
-        },
-        {
-            title: "eTaxBC",
-            image: imgETax,
-            description: "British Columbia's online tax portal. I spent several years as team lead for eTaxBC development.",
-            link: "https://www.etax.gov.bc.ca/btp/eservices/_/",
-            alt: "eTaxBC"
-        },
-        {
-            title: "Gentax",
-            image: imgGentax,
-            description: "Back end system for British Columbia's tax administration software. I've worked in most areas of the software, including the online tax portal eTaxBC, Audit, Discovery, Interfaces, etc.",
-            link: "https://fastenterprises.com",
-            alt: "Gentax"
-        },
-        {
-            title: "In a Nutshell: Bitcoin and the Blockchain",
-            image: imgAmazon,
-            description: "Introductory book on bitcoin and the blockchain, available as ebook and print on amazon. I published this book under my mother's maiden name Schulz (M. Schulz).",
-            link: "https://www.amazon.com/Nutshell-Bitcoin-Blockchain-M-Schulz/dp/1976722276/ref=sr_1_2?keywords=bitcoin+in+a+nutshell&qid=1562007175&s=books&sr=1-2",
-            alt: "In a Nutshell: Bitcoin and the Blockchain"
+    const data = useStaticQuery(graphql`
+        fragment fluidImage on File {
+            childImageSharp {
+                fluid(maxWidth: 500) {
+                    ...GatsbyImageSharpFluid
+                }
+            }
         }
-    ]
+        query {
+            blockstream: file(relativePath: { eq: "blockstream.png" }) {
+                ...fluidImage
+            }
+            explorer: file(relativePath: { eq: "block-explorer.png" }) {
+                ...fluidImage
+            }
+            store: file(relativePath: { eq: "blockstream-store.png" }) {
+                ...fluidImage
+            }
+            volkfi: file(relativePath: { eq: "volkfi.png" }) {
+                ...fluidImage
+            }
+            green: file(relativePath: { eq: "green.png" }) {
+                ...fluidImage
+            }
+            satellite: file(relativePath: { eq: "satellite.png" }) {
+                ...fluidImage
+            }
+            etax: file(relativePath: { eq: "etax.png" }) {
+                ...fluidImage
+            }
+            gentax: file(relativePath: { eq: "gentax.png" }) {
+                ...fluidImage
+            }
+            amazon: file(relativePath: { eq: "amazon.png" }) {
+                ...fluidImage
+            }
+        }
+        `)
+
+        const arrProjects = [
+            {
+                title: "blockstream.com",
+                image: data.blockstream.childImageSharp.fluid,
+                description: "Main Blockstream landing page. Built with Jekyll from the ground up as part of Blockstreams rebranding effort.",
+                link: "https://www.blockstream.com",
+                alt: "blockstream.com",
+                id: "blockstream"
+            },
+            {
+                title: "blockstream.info",
+                image: data.explorer.childImageSharp.fluid,
+                description: "Blockstream's block explorer, built using react.",
+                link: "https://www.blockstream.info",
+                alt: "blockstream.info",
+                id: "explorer"
+            },
+            {
+                title: "store.blockstream.com",
+                image: data.store.childImageSharp.fluid,
+                description: "Blockstream store is a wordpress store that showcases lightning payment technology.",
+                link: "https://store.blockstream.com",
+                alt: "store.blockstream.com",
+                id: "store"
+            },
+            {
+                title: "volkfi.com",
+                image: data.volkfi.childImageSharp.fluid,
+                description: "Volkfi landing page. I was involved as a contractor in the early efforts of building the site.",
+                link: "https://www.volkfi.com",
+                alt: "volkfi.com",
+                id: "volkfi"
+            },
+            {
+                title: "Blockstream Green (iOS)",
+                image: data.green.childImageSharp.fluid,
+                description: "Blockstream Green iOS wallet app.",
+                link: "https://www.blockstream.com/green",
+                alt: "Blockstream Green (iOS)",
+                id: "green"
+            }, 
+            {
+                title: "Blockstream Satellite Transmissions App",
+                image: data.satellite.childImageSharp.fluid,
+                description: "Blockstream Satellite transmissions app. This web app is available on blockstream.com and allows you to submit files and text messages via the blockstream satellite network. Payments are handled through the lightning network.",
+                link: "https://www.blockstream.com/satellite/",
+                alt: "Blockstream Satellite Transmissions App",
+                id: "satellite"
+            },
+            {
+                title: "eTaxBC",
+                image: data.etax.childImageSharp.fluid,
+                description: "British Columbia's online tax portal. I spent several years as team lead for eTaxBC development.",
+                link: "https://www.etax.gov.bc.ca/btp/eservices/_/",
+                alt: "eTaxBC",
+                id: "etax"
+            },
+            {
+                title: "Gentax",
+                image: data.gentax.childImageSharp.fluid,
+                description: "Back end system for British Columbia's tax administration software. I've worked in most areas of the software, including the online tax portal eTaxBC, Audit, Discovery, Interfaces, etc.",
+                link: "https://fastenterprises.com",
+                alt: "Gentax",
+                id: "gentax"
+            },
+            {
+                title: "In a Nutshell: Bitcoin and the Blockchain",
+                image: data.amazon.childImageSharp.fluid,
+                description: "Introductory book on bitcoin and the blockchain, available as ebook and print on amazon. I published this book under my mother's maiden name Schulz (M. Schulz).",
+                link: "https://www.amazon.com/Nutshell-Bitcoin-Blockchain-M-Schulz/dp/1976722276/ref=sr_1_2?keywords=bitcoin+in+a+nutshell&qid=1562007175&s=books&sr=1-2",
+                alt: "In a Nutshell: Bitcoin and the Blockchain",
+                id: "amazon"
+            }
+        ]
 
     const projectElements = arrProjects.map((project, index) => {
         return (
