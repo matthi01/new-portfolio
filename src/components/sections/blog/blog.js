faea86cfbdefea581e5f99e14d76bbcc0a2b65a2
Matthias Ruhland <matthias@blockstream.io>
2019-07-23 21:49:41 -0700

    initial set up

--- src/components/sections/blog/blog.js
+++ src/components/sections/blog/blog.js
@@ -15,7 +15,7 @@ const blog = () => {
                             author
                             title
                             path
-                            date
+                            date(formatString: "MMMM DD, YYYY")
                             excerpt
                         }
                     }
---
+++ src/data/posts/2019-07-23-gatsby-awesome.md
@@ -0,0 +1,9 @@
+---
+path: "/blog/gatsby-is-awesome"
+title: "Why Gatsby is Awesome"
+date: "2019-07-23"
+author: "Matthi"
+excerpt: "Why I love Gatsby (more than Jekyll)"
+---
+
+# Why Gatsby is Awesome
